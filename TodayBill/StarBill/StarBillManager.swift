cafeac386ba4a4b841a29e879bec6892bc1a5380
GeonH0 <h2jo369@naver.com>
2025-01-29 19:33:01 +0900

    Feat: 즐겨찾기 로직 선언

---
+++ TodayBill/StarBill/StarBillManager.swift
@@ -0,0 +1,53 @@
+//
+//  StarBillManager.swift
+//  TodayBill
+//
+//  Created by 김건호 on 1/28/25.
+//
+
+import Foundation
+
+final class StarBillManager {
+    static let shared = StarBillManager()
+    private let starredBillsKey = "starredBills"
+    private var cachedBills: [String: Row] = [:]
+
+    private init() {}
+    
+    func loadStarredBills() -> [StarredBill] {
+        if let data = UserDefaults.standard.data(forKey: starredBillsKey),
+           let bills = try? JSONDecoder().decode([StarredBill].self, from: data) {
+            return bills
+        }
+        return []
+    }
+    
+    func addBillToStarred(_ bill: StarredBill) {
+        var starredBills = loadStarredBills()
+        if !starredBills.contains(where: { $0.ID == bill.ID }) {
+            starredBills.append(bill)
+            saveStarredBills(starredBills)
+        }
+    }
+
+    // 즐겨찾기 제거
+    func removeBillFromStarred(by id: String) {
+        saveStarredBills(loadStarredBills().filter { $0.ID != id })
+    }
+
+    // 캐시된 데이터에 저장
+    func cacheBill(_ bill: Row) {
+        cachedBills[bill.BILL_ID] = bill
+    }
+    
+    func getBillFromCache(by id: String) -> Row? {
+        return cachedBills[id]
+    }
+
+    // UserDefaults 저장
+    private func saveStarredBills(_ bills: [StarredBill]) {
+        if let data = try? JSONEncoder().encode(bills) {
+            UserDefaults.standard.set(data, forKey: starredBillsKey)
+        }
+    }
+}
