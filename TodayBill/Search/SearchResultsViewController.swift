a20e2a52beed53dd3e6df8760b552622f4ad542e
GeonH0 <h2jo369@naver.com>
2025-01-29 21:31:17 +0900

    Fix: StarredBill 도입으로 인한 로직 수정, 검색 화면에서도 favoriteButtonTapped선언

--- TodayBill/Search/SearchResultsViewController.swift
+++ TodayBill/Search/SearchResultsViewController.swift
@@ -9,10 +9,10 @@ import Foundation
 import UIKit
 
 final class SearchResultsViewController: ListViewController {
-    private var searchResults: [String]
+    private var searchResults: [StarredBill]
 
     // MARK: - Initializer
-    init(searchResults: [String]) {
+    init(searchResults: [StarredBill]) {
         self.searchResults = searchResults
         super.init(items: searchResults)
     }
@@ -20,10 +20,46 @@ final class SearchResultsViewController: ListViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCollectionViewConstraints()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.favoriteItems = Set(StarBillManager.shared.loadStarredBills().map { $0.ID })
+        collectionView.reloadData()
+    }
+    
+    private func setupCollectionViewConstraints() {
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
 
     // MARK: - Update Results
-    func updateResults(_ results: [String]) {
+    
+    func updateResults(_ results: [StarredBill]) {
         self.searchResults = results
+        self.favoriteItems = Set(StarBillManager.shared.loadStarredBills().map { $0.ID })
         self.updateItems(searchResults)
+    }
+    
+    override func favoriteButtonTapped(for itemID: String) {
+        if favoriteItems.contains(itemID) {
+            favoriteItems.remove(itemID)
+            StarBillManager.shared.removeBillFromStarred(by: itemID)
+        } else {
+            favoriteItems.insert(itemID)
+            if let bill = items.first(where: { $0.ID == itemID }) {
+                StarBillManager.shared.addBillToStarred(bill)
+            }
+        }
+        collectionView.reloadData()
     }
 }
